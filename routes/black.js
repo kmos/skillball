2fd0a29264bf28f96761302b43e0403a72a795f6
Mosfet <giovanni.panice@gmail.com>
2019-01-26 12:02:32 +0100

    return value

--- routes/black.js
+++ routes/black.js
@@ -1,16 +1,22 @@
 /* post black listing. */
 router.post('/black/one_click', function(req, res, next) {
   console.log("one click from black!");
+
+  return "ok";
 });
 
 
 /* post black listing. */
 router.post('/black/double_click', function(req, res, next) {
   console.log("one click from black!");
+
+  return "ok";
 });
 
 
 /* post black listing. */
 router.post('/black/long_click', function(req, res, next) {
   console.log("one click from black!");
+
+  return "ok";
 });
