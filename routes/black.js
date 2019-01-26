d04c4d10dfbd2e05bbce8ab3d5deddb0065e218f
Mosfet <giovanni.panice@gmail.com>
2019-01-26 11:54:56 +0100

    add black button

---
+++ routes/black.js
@@ -0,0 +1,16 @@
+/* post black listing. */
+router.post('/black/oneClick', function(req, res, next) {
+  console.log("one click from black!");
+});
+
+
+/* post black listing. */
+router.post('/black/doubleClick', function(req, res, next) {
+  console.log("one click from black!");
+});
+
+
+/* post black listing. */
+router.post('/black/longClick', function(req, res, next) {
+  console.log("one click from black!");
+});
