3d77b330955e67c7a2e78f4ca851d9882b02559f
Mosfet <giovanni.panice@gmail.com>
2019-01-26 15:02:50 +0100

    fix send message

--- routes/black.js
+++ routes/black.js
@@ -4,21 +4,17 @@ var logger = require('heroku-logger');
 var wss = require('../bin/www');
 
 /* post black listing. */
+wss.on("connection", function(ws) {
+  ws.send("{\n" + "  \"player\": 0,\n" +
+    "  \"move\": \"left\"\n" +
+    "}");
+
+});
+
 router.post('/one_click', function(req, res, next) {
-
   logger.info("one click from black!");
+
   res.send("ok");
-
-  if(wss){
-    wss.on("connection", function(ws) {
-
-      ws.send("{\n" + "  \"player\": 0,\n" +
-        "  \"move\": \"left\"\n" +
-        "}");
-
-    });
-  }
-
 });
 
 
